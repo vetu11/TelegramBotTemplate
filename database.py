23d907ae90832bbac705e451928941c19442bc65
vetu11 <vetu@protonmail.com>
2019-10-02 14:48:59 +0200

    New method get_connetion() to support multithreading

--- database.py
+++ database.py
@@ -4,15 +4,30 @@ This class is an interface with the SQLite database.
 
 import sqlite3
 
+DATABASE_PATH = "database.db"
+
+
+class _DatabaseConnection:
+
+    def __init__(self, path=DATABASE_PATH):
+        self.conn = sqlite3.connect(path)
+
+    def __del__(self):
+        self.conn.close()
+
 
 class _Database:
 
     def __init__(self):
-        self.conn = sqlite3.connect("database.db")
+        self._perma_conn = sqlite3.connect(DATABASE_PATH)
 
     def execute_and_commit(self, sql: str, parameters: iter):
-        self.conn.execute(sql, parameters)
-        self.conn.commit()
+        self._perma_conn.execute(sql, parameters)
+        self._perma_conn.commit()
+
+    @staticmethod
+    def get_connection():
+        return _DatabaseConnection()
 
 
 database = _Database()
