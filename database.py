efe5e0ddb35704ad7f4ae3662666360ace335eb6
vetu11 <vetu@protonmail.com>
2019-10-02 16:42:16 +0200

    Helper methods to fetch elements fetch elements from the database

--- database.py
+++ database.py
@@ -29,5 +29,29 @@ class _Database:
     def get_connection():
         return _DatabaseConnection()
 
+    @staticmethod
+    def get_one_fetched_as_dict(cursor):
+        desc = cursor.description
+        row = cursor.fetchone()
+        new_dict = {}
+
+        for i in range(len(row)):
+            new_dict[desc[i][0]] = row[i]
+        return new_dict
+
+    @staticmethod
+    def get_all_fetched_as_dict(cursor):
+        desc = cursor.description
+        lst = cursor.fetchall()
+        fetched = []
+
+        for row in lst:
+            new_dict = {}
+            for i in range(len(row)):
+                new_dict[desc[i][0]] = row[i]
+            fetched.append(new_dict)
+
+        return fetched
+
 
 database = _Database()
